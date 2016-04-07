b00d525df50adbb7b36aeee8a8149448d1c47eb9
zangxd <zangxd@126.com>
2016-04-07 15:22:25 +0800

    But all sunshine without shade,all pleasure withour pain,is not life at all

--- src/js/page/taskland/taskland.js
+++ src/js/page/taskland/taskland.js
@@ -27,5 +27,21 @@
     $scope.duetodayList = false;
     $scope.otherdueList = false;
  });
+tasklandModule.controller('paginationCtrl', function ($scope, $log) {
+  $scope.totalItems = 99;
+  $scope.currentPage = 1;
+
+  $scope.setPage = function (pageNo) {
+    $scope.currentPage = pageNo;
+  };
+
+  $scope.pageChanged = function() {
+    $log.log('Page changed to: ' + $scope.currentPage);
+  };
+
+  $scope.maxSize = 5; //最大显示页数
+  $scope.bigTotalItems = 175;// 跟totalItems差不多最大记录数
+  $scope.bigCurrentPage = 1;// 默认显示第几页
+});
 
  export default tasklandModule;
